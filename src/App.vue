1439315e10a780f912e047ee26a42550bda986d5
IlyaGlukhovskiy <g.ilya2003@mail.ru>
2024-12-07 00:19:44 +0300

    code decoration

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="wrapper">
-    <h1> Знаю погоду не смотря в окно </h1>
-    <p> Покажем погоду в {{ city == "" ? "вашем городе" : "`" + city + "`" }} </p>
+  <div class="field">
+    <!--main text-->
+    <h1> I know the weather without looking out the window </h1>
+    <p> We'll show you the weather {{ city == "" ? "in your city" : "`" + city + "`" }} </p>
 
-    <input type="text" placeholder="Москва" v-model="city">
+    <!--input field-->
+    <input type="text" placeholder="Moscow" v-model="city">
 
-    <button v-if="city!=''" @click="getWeather()"> Узнать погоду </button>
-    <button disabled v-else=""> Укажите город </button>
+    <!--button settings-->
+    <button v-if="city!=''" @click="getWeather()"> Check the weather </button>
+    <button disabled v-else=""> Please enter a city </button>
 
+    <!--output data-->
     <div v-if="info != null">
       <p> {{ cityName }} </p>
       <p> {{ cityTemp }} </p>
@@ -15,13 +19,14 @@
       <p> {{ cityMinTemp }} </p>
       <p> {{ cityMaxTemp }} </p>
     </div>
-
+    
+    <!--error field-->
     <p class="error"> {{ error }} </p>
   </div>
 </template>
 
 <style scoped>
-  .wrapper{
+  .field{
     width: 900px;
     height: 500px;
     padding: 20px;
@@ -31,15 +36,15 @@
     color: white;
   }
 
-  .wrapper h1{
+  .field h1{
     margin-top: 50px;
   }
 
-  .wrapper p {
+  .field p {
     margin-top: 20px;
   }
 
-  .wrapper input{
+  .field input{
     margin-top: 30px;
     background: transparent;
     border: 0;
@@ -50,11 +55,11 @@
     outline: none;
   }
 
-  .wrapper input:focus{
+  .field input:focus{
     border-bottom-color: #6e2d7d;
   }
 
-  .wrapper button{
+  .field button{
     padding: 10px 15px;
     margin-left: 20px;
     border: 2px solid #b99935;
@@ -65,11 +70,11 @@
     transition: transform 500ms ease;
   }
 
-  .wrapper button:hover{
+  .field button:hover{
     transform: scale(1.1) translateY(-3px);
   }
 
-  .wrapper button:disabled{
+  .field button:disabled{
     background: #746027;
     cursor: not-allowed;
     transition: none;
@@ -83,6 +88,7 @@
 
 <script>
 import axios from "axios";
+//enter variables
 export default {
   data() {
     return {
@@ -92,24 +98,26 @@ export default {
     };
   },
 
+  // output data
   computed: {
     cityName() {
       return "`" + this.city + "`";
     },
     cityTemp() {
-      return "Температура: " + this.info.main.temp + "°C";
+      return "Temperature: " + this.info.main.temp + "°C";
     },
     cityFeelsLike() {
-      return "Ощущается как: " + this.info.main.feels_like + "°C";
+      return "Feels like: " + this.info.main.feels_like + "°C";
     },
     cityMinTemp() {
-      return "Минимальная температура: " + this.info.main.temp_min + "°C";
+      return "Min temperature: " + this.info.main.temp_min + "°C";
     },
     cityMaxTemp() {
-      return "Максимальная температура: " + this.info.main.temp_max + "°C";
+      return "Max temperature: " + this.info.main.temp_max + "°C";
     },
   },
 
+  //cleaning output data
   watch: {
     city(newCity) {
       if (newCity.trim() === "") {
@@ -119,10 +127,11 @@ export default {
     
   },
 
+  //error handler + link
   methods: {
     getWeather() {
       if (this.city.trim().length < 2) {
-        this.error = "Название города не может состоять из одной буквы!";
+        this.error = "The name of the city cannot consist of one letter!";
         return false;
       }
       this.error = "";
@@ -133,7 +142,7 @@ export default {
         )
         .then((res) => (this.info = res.data))
         .catch(() => {
-          this.error = "Не удалось получить данные. Проверьте название города.";
+          this.error = "Failed to retrieve data. Check the city name.";
         });
     },
   },
